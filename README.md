202cc242eeab11b8b2c9032512f07af43634dfc9
Bruce <recall4056@gmail.com>
2023-06-02 14:48:33 +0800

    chore: clear project

--- README.md
+++
@@ -1,5 +0,0 @@
-# Yahoo News App
-
-## License
-
-[MIT](/LICENSE) License &copy; 2023 [Bruce Song](https://github.com/recallwei)
--- src/shims.d.ts
+++
@@ -1,7 +0,0 @@
-// Fix the error when import the .vue file in .ts file.
-declare module "*.vue" {
-  import { type DefineComponent } from "vue"
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
-declare module "*"
