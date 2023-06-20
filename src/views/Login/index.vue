7c5d1fa49fac315495f5bd64bb166ad9a722f799
Bruce <recall4056@gmail.com>
2023-06-21 00:24:37 +0800

    Revert "Merge branch 'main' of https://github.com/recallwei/yahoo-news-app"
    This reverts commit 72bd5a358f4943445033a5adb8663e47548ba176, reversing
    changes made to 1affd299c9367e9a9179f34ad82fb8c8aaec1165.

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { LoginApi } from '@/api'
+import { siteMetaData } from '@/constants'
 import { setToken } from '@/utils'
 
 type RememberedAccountData = {
@@ -8,6 +9,8 @@ type RememberedAccountData = {
 }
 
 const REMEMBERED_ACCOUNT_DATA_KEY = 'user_password'
+
+const { appName, version } = siteMetaData
 
 const router = useRouter()
 const message = useMessage()
@@ -80,63 +83,80 @@ onMounted(() => {
 </script>
 
 <template>
-  <form
-    class="absolute inset-0 m-auto flex h-fit w-[340px] max-w-[85%] flex-col space-y-4 rounded-lg bg-white px-4 py-8 shadow-md sm:w-[260px] md:w-[340px]"
-  >
-    <div class="text-center text-lg font-semibold text-gray-600">登录</div>
-    <n-input
-      v-model:value="formData.username"
-      type="text"
-      placeholder="账号"
-      :input-props="{ autocomplete: 'username' }"
-      @keyup.enter="() => loginAsBasic()"
-    />
-    <n-input
-      v-model:value="formData.password"
-      type="password"
-      show-password-on="mousedown"
-      placeholder="密码"
-      :maxlength="16"
-      :input-props="{ autocomplete: 'current-password' }"
-      @keyup.enter="() => loginAsBasic()"
-    />
-
-    <div class="text-grey-300 flex items-center justify-between text-xs font-light">
-      <n-checkbox
-        v-model:checked="rememberPassword"
-        size="small"
-        class="!text-xs"
-      >
-        记住密码
-      </n-checkbox>
-      <div
-        class="cursor-pointer hover:text-blue-600"
-        @click="forgotPassword"
-      >
-        忘记密码
+  <main class="flex h-screen w-screen">
+    <div class="invisible relative w-0 bg-blue-200 sm:visible sm:w-1/2">
+      <div class="absolute inset-0 m-auto flex h-fit w-fit select-none flex-col space-y-2 text-center font-mono">
+        <img
+          src="@/assets/images/favicon.png"
+          alt=""
+          width="180"
+          height="180"
+        />
+        <span class="text-xl font-semibold">{{ appName }}</span>
+        <span class="text-base">v{{ version }}</span>
       </div>
     </div>
 
-    <n-button
-      type="primary"
-      @click="() => loginAsBasic()"
-    >
-      登录
-    </n-button>
-    <n-button
-      type="info"
-      @click="() => loginAsAdmin()"
-    >
-      以管理员登录
-    </n-button>
-    <n-button
-      text
-      size="tiny"
-      @click="() => router.push('/signup')"
-    >
-      注册
-    </n-button>
-  </form>
+    <div class="relative w-full bg-blue-300 sm:w-1/2">
+      <form
+        class="absolute inset-0 m-auto flex h-fit w-[340px] max-w-[85%] flex-col space-y-4 rounded-lg bg-white px-4 py-8 shadow-md sm:w-[260px] md:w-[340px]"
+      >
+        <div class="text-center text-lg font-semibold text-gray-600">登录</div>
+        <n-input
+          v-model:value="formData.username"
+          type="text"
+          placeholder="账号"
+          :input-props="{ autocomplete: 'username' }"
+          @keyup.enter="() => loginAsBasic()"
+        />
+        <n-input
+          v-model:value="formData.password"
+          type="password"
+          show-password-on="mousedown"
+          placeholder="密码"
+          :maxlength="16"
+          :input-props="{ autocomplete: 'current-password' }"
+          @keyup.enter="() => loginAsBasic()"
+        />
+
+        <div class="text-grey-300 flex items-center justify-between text-xs font-light">
+          <n-checkbox
+            v-model:checked="rememberPassword"
+            size="small"
+            class="!text-xs"
+          >
+            记住密码
+          </n-checkbox>
+          <div
+            class="cursor-pointer hover:text-blue-600"
+            @click="forgotPassword"
+          >
+            忘记密码
+          </div>
+        </div>
+
+        <n-button
+          type="primary"
+          @click="() => loginAsBasic()"
+        >
+          登录
+        </n-button>
+        <n-button
+          type="info"
+          @click="() => loginAsAdmin()"
+        >
+          以管理员登录
+        </n-button>
+        <n-button
+          text
+          size="tiny"
+          @click="() => router.push('/signup')"
+        >
+          注册
+        </n-button>
+      </form>
+    </div>
+  </main>
 </template>
 
 <style scoped lang="scss">
