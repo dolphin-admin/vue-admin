037fcbfbc972891cd623f4ae58848fdca9523c2e
Upwards-rwr <1512501934@qq.com>
2023-06-25 23:56:30 +0800

    feat: layoutEnhanced

--- src/components/BaseHeader/index.vue
+++ src/components/BaseHeader/index.vue
@@ -1,22 +1,38 @@
 <script setup lang="ts">
 import { UserApi } from '@/api'
-import { useThemeStore } from '@/store'
-import type { MessageSchema, User } from '@/types'
-import { clearLang, clearTheme, clearToken } from '@/utils'
+import { useSidebarStore, useThemeStore } from '@/store'
+import type { Lang, MessageSchema, User } from '@/types'
+import { clearLang, clearTheme, clearToken, setLang } from '@/utils'
 import NotificationIcon from '~icons/ic/baseline-notifications-none'
 import LanguageIcon from '~icons/ion/language-outline'
-import SunIcon from '~icons/line-md/moon-to-sunny-outline-transition'
+import MenuFoldLeftIcon from '~icons/line-md/menu-fold-left'
+import SunIcon from '~icons/line-md/moon-alt-to-sunny-outline-loop-transition'
 import MoonIcon from '~icons/line-md/sunny-filled-loop-to-moon-alt-filled-loop-transition'
 import FullScreenIcon from '~icons/material-symbols/fullscreen'
+import SettingIcon from '~icons/material-symbols/settings-outline-rounded'
 
 const themeStore = useThemeStore()
+const sidebarStore = useSidebarStore()
 const router = useRouter()
 const message = useMessage()
 // @ts-ignore
 const { t } = useI18n<{ message: MessageSchema }>({ useScope: 'global' })
 
 const userInfo = reactive<Partial<User>>({})
-
+const options = [
+  { label: 'English', key: 'en_US' },
+  { label: '简体中文', key: 'zh_CN' }
+]
+const optionsUser = [
+  {
+    label: '锁定屏幕',
+    key: 'marina bay sands'
+  },
+  {
+    label: '退出系统',
+    key: 'marina bay sands'
+  }
+]
 const logout = () => {
   clearToken()
   clearLang()
@@ -38,49 +54,109 @@ onBeforeMount(() => {
     .catch(() => message.error(t('UserInfo.FetchError')))
     .finally(() => loadingMessage.destroy())
 })
+
+const handleUpdateLocale = (lang: Lang) => {
+  themeStore.changeLocale(lang)
+  setLang(lang)
+  // locale.value = lang
+}
+
+const handleCollapseMenu = () => {
+  sidebarStore.changeSidebarStatus(true)
+}
 </script>
 
 <template>
   <header
-    class="z-50 flex h-16 w-full items-center justify-between border-b border-gray-300 bg-light-default p-4 shadow-sm dark:border-gray-950 dark:bg-dark-default"
+    class="sticky top-0 z-50 flex h-16 w-full items-center justify-between border-b border-gray-300 bg-light-default p-4 shadow-sm dark:border-gray-950 dark:bg-dark-default"
   >
-    <div class="flex h-full items-center justify-start"></div>
+    <div
+      class="flex h-full items-center justify-start"
+      @click="handleCollapseMenu"
+    >
+      <component :is="MenuFoldLeftIcon"></component>
+    </div>
 
     <div class="flex h-full items-center justify-end space-x-4">
-      <NotificationIcon class="cursor-pointer dark:text-white" />
-
-      <FullScreenIcon
-        class="cursor-pointer dark:text-white"
-        height="24"
-        width="24"
-      />
-
-      <LanguageIcon class="cursor-pointer dark:text-white" />
-
-      <SunIcon
-        v-if="themeStore.themeMode === 'light'"
-        class="cursor-pointer dark:text-white"
-        @click="() => themeStore.changeThemeMode(themeStore.themeMode === 'light' ? 'dark' : 'light')"
-      />
-      <MoonIcon
-        v-else
-        class="cursor-pointer dark:text-white"
-        @click="() => themeStore.changeThemeMode(themeStore.themeMode === 'light' ? 'dark' : 'light')"
-      />
-
-      <div
-        v-if="userInfo.id"
-        class="flex select-none items-center space-x-3"
+      <NTooltip
+        placement="bottom"
+        trigger="hover"
       >
-        <img
-          v-if="userInfo.avatarUrl"
-          class="h-8 w-8 cursor-pointer rounded-full"
-          :src="userInfo.avatarUrl"
-          alt=""
-          @click="() => logout()"
-        />
-        <span class="text-sm">{{ userInfo.username }}</span>
-      </div>
+        <template #trigger>
+          <NotificationIcon class="cursor-pointer dark:text-white" />
+        </template>
+        <span class="dark:text-white">{{ t('HeaderInfo.Notification') }}</span>
+      </NTooltip>
+      <NTooltip
+        placement="bottom"
+        trigger="hover"
+      >
+        <template #trigger>
+          <FullScreenIcon
+            class="cursor-pointer dark:text-white"
+            height="24"
+            width="24"
+          />
+        </template>
+        <span class="dark:text-white">{{ t('HeaderInfo.FullScreen') }}</span>
+      </NTooltip>
+      <NDropdown
+        trigger="click"
+        :options="options"
+        @select="handleUpdateLocale"
+      >
+        <NTooltip
+          placement="bottom"
+          trigger="hover"
+        >
+          <template #trigger>
+            <LanguageIcon class="cursor-pointer dark:text-white" />
+          </template>
+          <span class="dark:text-white">{{ t('HeaderInfo.language') }}</span>
+        </NTooltip>
+      </NDropdown>
+      <NTooltip
+        placement="bottom"
+        trigger="hover"
+      >
+        <template #trigger>
+          <SunIcon
+            v-if="themeStore.themeMode === 'light'"
+            class="cursor-pointer dark:text-white"
+            @click="() => themeStore.changeThemeMode(themeStore.themeMode === 'light' ? 'dark' : 'light')"
+          />
+          <MoonIcon
+            v-else
+            class="cursor-pointer dark:text-white"
+            @click="() => themeStore.changeThemeMode(themeStore.themeMode === 'light' ? 'dark' : 'light')"
+          />
+        </template>
+        <span class="dark:text-white">{{ t('HeaderInfo.SwitchingTheme') }}</span>
+      </NTooltip>
+      <NDropdown
+        :options="optionsUser"
+        trigger="hover"
+      >
+        <div class="flex select-none items-center space-x-3">
+          <img
+            v-if="userInfo.avatarUrl"
+            class="h-8 w-8 cursor-pointer rounded-full"
+            :src="userInfo.avatarUrl"
+            alt=""
+            @click="() => logout()"
+          />
+          <span class="text-sm">{{ userInfo.username }}</span>
+        </div>
+      </NDropdown>
+      <NTooltip
+        placement="bottom"
+        trigger="hover"
+      >
+        <template #trigger>
+          <SettingIcon class="cursor-pointer dark:text-white" />
+        </template>
+        <span class="dark:text-white">{{ t('HeaderInfo.Setting') }}</span>
+      </NTooltip>
     </div>
   </header>
 </template>
--- src/components/BaseSidebar/index.vue
+++ src/components/BaseSidebar/index.vue
@@ -1,7 +1,36 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useSidebarStore, useThemeStore } from '@/store'
+
+const themeStore = useThemeStore()
+const sidebarStore = useSidebarStore()
+
+// @ts-ignore
+const { t } = useI18n<{ message: MessageSchema }>({ useScope: 'global' })
+</script>
 
 <template>
   <div
-    class="z-50 h-full w-40 border-r border-gray-300 bg-light-default shadow-sm dark:border-gray-950 dark:bg-dark-default"
-  ></div>
+    :class="`
+    h-full
+    w-${sidebarStore.isCollapse ? 10 : 64}
+    border-r
+    border-gray-300
+    bg-light-default
+    shadow-sm
+    dark:border-gray-950
+    dark:bg-dark-default`"
+  >
+    <div class="flex h-16 w-full items-center justify-center">
+      <img
+        class="animate-pulse cursor-pointer select-none"
+        width="32"
+        height="32"
+        src="@/assets/images/favicon.png"
+        alt=""
+        @click="() => themeStore.changeThemeMode(themeStore.themeMode === 'light' ? 'dark' : 'light')"
+      />
+      <span class="whitespace-nowrap text-xs font-semibold">{{ t('App.Name') }}</span>
+    </div>
+    <div class="w-full"></div>
+  </div>
 </template>
