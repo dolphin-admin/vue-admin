dc40a873d954c93a843144bead527c34301474e6
Bruce <recall4056@gmail.com>
2023-06-26 10:20:34 +0800

    refactor: vite config load from .env

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,9 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  readonly VITE_PORT: string
   readonly VITE_API_PREFIX: string
+  readonly VITE_API_PROXY: string
 }
 
 interface ImportMeta {
