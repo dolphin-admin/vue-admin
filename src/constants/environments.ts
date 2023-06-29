42068d616cd91b8445aac49c9391741a5248cbf8
Bruce <recall4056@gmail.com>
2023-06-29 10:41:44 +0800

    style: format

--- src/constants/environments.ts
+++ src/constants/environments.ts
@@ -1,6 +1,6 @@
 export const GlobalEnvConfig = Object.freeze({
-  API_PREFIX: import.meta.env.VITE_API_PREFIX ?? '',
   PORT: import.meta.env.VITE_PORT ?? '',
+  API_PREFIX: import.meta.env.VITE_API_PREFIX ?? '',
   API_PROXY: import.meta.env.VITE_API_PROXY ?? '',
   MODE: import.meta.env.MODE,
   IS_DEV: import.meta.env.DEV,
