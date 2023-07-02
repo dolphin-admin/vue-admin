e8cdbb6954037495cb81ef85ce0907d526bc960f
Bruce Song <recall4056@gmail.com>
2023-07-02 08:42:18 +0800

    feat(BaseLayout): :sparkles: add router change transition

--- src/components.d.ts
+++ src/components.d.ts
@@ -23,6 +23,7 @@ declare module 'vue' {
     NForm: typeof import('naive-ui')['NForm']
     NFormItem: typeof import('naive-ui')['NFormItem']
     NIcon: typeof import('naive-ui')['NIcon']
+    NImage: typeof import('naive-ui')['NImage']
     NInput: typeof import('naive-ui')['NInput']
     NLoadingBarProvider: typeof import('naive-ui')['NLoadingBarProvider']
     NMenu: typeof import('naive-ui')['NMenu']
