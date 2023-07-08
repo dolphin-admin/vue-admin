71dddb91c75ab328337717d017c4f2d299fae9e9
Upwards-rwr <1512501934@qq.com>
2023-07-08 21:40:21 +0800

    fix: :bug: clipboard

--- src/views/Clipboard/index.vue
+++ src/views/Clipboard/index.vue
@@ -1,35 +1,51 @@
 <script setup lang="ts">
 import type { MessageSchema } from '@/types'
-const message = useMessage()
 
+const message = useMessage()
+const [loading, loadingDispatcher] = useLoading(false)
 const { t } = useI18n<{ message: MessageSchema }>({ useScope: 'global' })
 
 const clipboardValue = ref(
-  '散落的月光穿过了云  躲着人群 铺成大海的鳞 海浪打湿白裙 试图推你回去 海浪清洗血迹 妄想温暖你 往海的深处听 谁的哀鸣在指引 灵魂没入寂静 无人将你吵醒 你喜欢海风咸咸的气息 踩着湿湿的沙砾 你说人们的骨灰应该撒进海里'
+  '散落的月光穿过了云，躲着人群，铺成大海的鳞，海浪打湿白裙，试图推你回去，海浪清洗血迹，妄想温暖你，往海的深处听，谁的哀鸣在指引，灵魂没入寂静，无人将你吵醒，你喜欢海风咸咸的气息，踩着湿湿的沙砾，你说人们的骨灰应该撒进海里。'
 )
-const clipText = () => {
-  BrowserUtils.setClipBoardText(clipboardValue.value)
-  message.success(t('Message.Copy.CopySuccessfully'))
+
+const clipText = async () => {
+  loadingDispatcher.loading()
+  try {
+    BrowserUtils.setClipBoardText(clipboardValue.value)
+    message.success(t('Message.Copy.Success'))
+  } catch {
+    message.error(t('Message.Copy.Failed'))
+  }
+  loadingDispatcher.loaded()
 }
 </script>
 
 <template>
   <main>
-    <NCard>
+    <NCard class="flex w-full flex-col space-y-2">
       <NInput
         v-model:value="clipboardValue"
-        class="min-h-[100px]"
         type="textarea"
         autofocus
+        show-count
         clearable
-        autosize
+        :autosize="{
+          minRows: 8,
+          maxRows: 20
+        }"
       />
-      <NButton
-        type="primary"
-        class="!mt-3"
-        @click="clipText"
-        >{{ t('Clipboard.CopyToClipboard') }}</NButton
-      >
+      <div class="flex items-center justify-center">
+        <NButton
+          type="primary"
+          :loading="loading"
+          :disabled="loading"
+          class="!mt-3"
+          @click="clipText"
+        >
+          {{ t('Clipboard.CopyToClipboard') }}
+        </NButton>
+      </div>
     </NCard>
   </main>
 </template>
