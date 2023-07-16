0b09e3732577f3acf060e551d1e9c6630ec2691c
Bruce Song <recall4056@gmail.com>
2023-07-16 23:24:54 +0800

    chore: update

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,6 +7,7 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
+    '(chore': update)
     AuthLayout: typeof import('./layouts/AuthLayout/index.vue')['default']
     BarChart: typeof import('./charts/BarChart/index.vue')['default']
     BaseBreadcrumb: typeof import('./components/BaseBreadcrumb/index.vue')['default']
