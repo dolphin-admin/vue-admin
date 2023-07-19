252ad165d45805a74846bf240098e77631c4cd91
Bruce Song <recall4056@gmail.com>
2023-07-19 08:44:19 +0800

    feat: update socket demo page

--- src/views/SystemTools/WebSocket/index.vue
+++ src/views/SystemTools/WebSocket/index.vue
@@ -5,8 +5,31 @@ import { io } from 'socket.io-client'
 const userStore = useUserStore()
 const message = useMessage()
 
-const socket = ref<Socket | null>(null)
+const socket = ref<Socket>(
+  io(`${GlobalEnvConfig.BASE_API_PROXY}/demo`, {
+    autoConnect: false // 禁止自动连接
+  })
+)
 
+/**
+ * 建立连接
+ */
+const handleConnect = () => {
+  socket.value.connect()
+  socket.value.emit('join', `${userStore.user.username} 已建立连接`)
+}
+
+/**
+ * 断开连接
+ */
+const handleDisconnect = () => {
+  socket.value.emit('leave', `${userStore.user.username} 已断开连接`)
+  socket.value.disconnect()
+}
+
+/**
+ * 发送消息
+ */
 const handleSendMessage = () => {
   const sendMessage = `你好，我是${userStore.user.username}，我刚刚给你发送了一条信息`
   socket.value!.emit('message', sendMessage)
@@ -14,24 +37,55 @@ const handleSendMessage = () => {
 }
 
 onBeforeMount(() => {
-  socket.value = io(GlobalEnvConfig.BASE_API_PROXY + '/websocket')
+  // 监听连接
   socket.value.on('connect', () => {
-    console.log('connect')
+    message.success('已建立 socket 连接')
   })
+  // 监听断开连接
   socket.value.on('disconnect', () => {
-    console.log('disconnect')
+    message.success('已断开 socket 连接')
   })
+  // 监听服务端发送的消息
   socket.value.on('message', (data) => {
-    console.log(data)
+    message.success(data)
+  })
+  // 监听用户加入
+  socket.value.on('join', (data) => {
+    message.success(data)
+  })
+  // 监听用户离开
+  socket.value.on('leave', (data) => {
     message.success(data)
   })
 })
 
-onBeforeUnmount(() => socket.value!.disconnect())
+// 组件销毁时断开连接
+onBeforeUnmount(() => socket.value.disconnect())
 </script>
 
 <template>
   <main>
-    <NButton @click="handleSendMessage">发送信息</NButton>
+    <NCard hoverable>
+      <div class="flex items-center space-x-4">
+        <NButton
+          :disabled="socket.connected"
+          @click="handleConnect"
+        >
+          建立连接
+        </NButton>
+        <NButton
+          :disabled="socket.disconnected"
+          @click="handleDisconnect"
+        >
+          断开连接
+        </NButton>
+        <NButton
+          :disabled="socket.disconnected"
+          @click="handleSendMessage"
+        >
+          发送信息
+        </NButton>
+      </div>
+    </NCard>
   </main>
 </template>
