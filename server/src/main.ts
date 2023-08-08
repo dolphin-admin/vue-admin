1bce496748c207e4b8205ab614a700a7dbe39598
Bruce <recall4056@gmail.com>
2023-08-08 10:49:37 +0800

    chore: add generate crud api module script

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core'
+import type { NestExpressApplication } from '@nestjs/platform-express'
 
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    abortOnError: false
+  })
   await app.listen(3000)
 }
 bootstrap()
