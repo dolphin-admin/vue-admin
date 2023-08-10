32497df83b39f84af9b2b74e8d1ff9b44516850e
Bruce <recall4056@gmail.com>
2023-08-10 12:59:55 +0800

    build: update build script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,5 +3,5 @@ git checkout main &&
   git pull &&
   pnpm i &&
   pnpm server:build &&
-  pm2 restart server/dist/main.js --name dolphin-admin-api &&
+  pnpm pm2 restart server/dist/main.js --name dolphin-admin-api &&
   exit
