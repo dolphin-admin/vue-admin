a89ac581bab366ea1355de5df4501f9dbcf58582
Bruce <recall4056@gmail.com>
2023-08-10 10:39:39 +0800

    chore: update github action scripts

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
-cd /home/repo/dolphin-admin/ || exit
-git checkout main
-git pull
-pnpm i
-pnpm server:build
-pm2 restart server/dist/main.js --name dolphin-admin-api
+cd /home/repo/dolphin-admin/ &&
+  git checkout main &&
+  git pull &&
+  pnpm i &&
+  pnpm server:build &&
+  pm2 restart server/dist/main.js --name dolphin-admin-api &&
+  exit
