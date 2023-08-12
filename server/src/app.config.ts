1f2ff7eda6f064ad22aa1a3e04b9ce3260e163f4
Bruce <recall4056@gmail.com>
2023-08-13 00:44:09 +0800

    feat: add appconfig for site metadata

---
+++ server/src/app.config.ts
@@ -0,0 +1,11 @@
+import type { SiteMetadata } from '@/types'
+
+export const AppConfig: SiteMetadata = Object.freeze({
+  appName: 'Dolphin Admin',
+  version: '0.0.1',
+  teamName: 'Bit Ocean',
+  teamGitHubURL: 'https://github.com/bit-ocean-studio',
+  repoGitHubURL: 'https://github.com/bit-ocean-studio/dolphin-admin-web',
+  docsURL: 'https://dolphin-admin-docs.bit-ocean.studio',
+  discordURL: 'https://discord.gg/UKhpUQgwCT'
+})
---
+++ server/src/types/site-metadata.ts
@@ -0,0 +1,9 @@
+export interface SiteMetadata {
+  appName: string
+  version: string
+  teamName: string
+  teamGitHubURL: string
+  repoGitHubURL: string
+  docsURL: string
+  discordURL: string
+}
