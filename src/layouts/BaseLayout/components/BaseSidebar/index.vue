9ca77ec0fb36bbb3d0ad2c0fa5efbedb7a28c615
Bruce <recall4056@gmail.com>
2023-11-18 21:07:14 +0800

    fix: :bug: border display overlap

--- src/layouts/BaseLayout/components/BaseSidebar/index.vue
+++ src/layouts/BaseLayout/components/BaseSidebar/index.vue
@@ -35,7 +35,9 @@ watch(
 <template>
   <div
     class="absolute inset-y-0 left-0 z-[100] h-full border border-gray-300 bg-default-light shadow-sm transition-[width] dark:border-gray-950 dark:bg-default-dark sm:static"
-    :class="[sidebarStore.isDisplay ? (sidebarStore.isCollapse ? 'w-16' : 'w-56') : 'w-0']"
+    :class="[
+      sidebarStore.isDisplay ? (sidebarStore.isCollapse ? 'w-16' : 'w-56') : 'w-0 border-r-0'
+    ]"
   >
     <!-- Header -->
     <div
