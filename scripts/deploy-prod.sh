49fc97358bc3e31a852acc5dd15efe9fa3d314ea
Bruce <recall4056@gmail.com>
2023-12-18 00:06:16 +0800

    chore: :hammer: update deps

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -4,11 +4,8 @@ set -o allexport
 source .env.production
 set +o allexport
 
-set -e
-node scripts/generate-deploy-info.js
-
 pnpm i
 pnpm build:prod
+echo "正在上传静态资源至 $SERVER_IP"
 scp -r dist "$SERVER_USER"@"$SERVER_IP":/usr/share/nginx/html/dolphin-admin-vue/
-
 echo "成功部署至 $SERVER_IP"
--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -4,11 +4,8 @@ set -o allexport
 source .env.staging
 set +o allexport
 
-set -e
-node scripts/generate-deploy-info.js
-
 pnpm i
 pnpm build:staging
+echo "正在上传静态资源至 $SERVER_IP"
 scp -r dist "$SERVER_USER"@"$SERVER_IP":/usr/share/nginx/html/dolphin-admin-vue/
-
 echo "成功部署至 $SERVER_IP"
